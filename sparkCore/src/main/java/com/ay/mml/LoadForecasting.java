19659dd4c3b55f49c6e48e7d55843e203ec68ab6
ay_glb <960484339@qq.com>
2019-10-07 00:15:38 +0800

    mml

--- sparkCore/src/main/java/com/ay/mml/LoadForecasting.java
+++ sparkCore/src/main/java/com/ay/mml/LoadForecasting.java
@@ -7,16 +7,14 @@ package com.ay.mml;
 
 // $example on$
 import com.microsoft.ml.spark.lightgbm.LightGBMRegressor;
-import org.apache.spark.ml.Model;
-import org.apache.spark.ml.Pipeline;
-import org.apache.spark.ml.PipelineModel;
-import org.apache.spark.ml.PipelineStage;
+import org.apache.spark.ml.*;
 import org.apache.spark.ml.evaluation.RegressionEvaluator;
 import org.apache.spark.ml.feature.VectorAssembler;
 import org.apache.spark.ml.feature.VectorIndexer;
 import org.apache.spark.ml.feature.VectorIndexerModel;
 import org.apache.spark.ml.param.Param;
 import org.apache.spark.ml.param.ParamMap;
+import org.apache.spark.ml.param.ParamPair;
 import org.apache.spark.ml.regression.GBTRegressionModel;
 import org.apache.spark.ml.regression.GBTRegressor;
 import org.apache.spark.ml.tuning.CrossValidator;
@@ -25,6 +23,8 @@ import org.apache.spark.ml.tuning.ParamGridBuilder;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+
+import java.util.List;
 // $example off$
 
 public class LoadForecasting {
@@ -32,7 +32,9 @@ public class LoadForecasting {
         SparkSession spark = SparkSession
                 .builder()
                 .appName("javaMML")
-                .config("spark.master","local[*]")
+                .config("spark.executor.cores",4)
+                .config("spark.default.parallelism", 100)
+//                .config("spark.master","local[*]")
 //                .config("spark.eventLog.enabled", "false")
                 .getOrCreate();
         Dataset<Row> df = spark
@@ -41,8 +43,8 @@ public class LoadForecasting {
                 .option("header", "true")
                 .option("multiLine", true)
                 .option("inferSchema", true)
-//                .load("/full_features_shift.csv");
-                .load("file:///H:\\jupyter\\energy_forecasting_notebooks\\full_features_shift.csv");
+                .load("/full_features_shift.csv");
+//                .load("file:///H:\\jupyter\\energy_forecasting_notebooks\\full_features_shift.csv");
 
         // $example on$
         // Load and parse the data file, converting it to a DataFrame.
@@ -70,9 +72,10 @@ public class LoadForecasting {
 
         ParamMap[] paramGridBuilder = new ParamGridBuilder()
 //                .addGrid(lightGBMRegressor.maxDepth(), new int[]{3, 4, 5,6,7,8})
-//                .addGrid(lightGBMRegressor.numLeaves(), new int[]{16, 20, 31, 40, 60})
+                .addGrid(lightGBMRegressor.numLeaves(), new int[]{16, 20, 31, 40, 60})
                 .addGrid(lightGBMRegressor.learningRate(),new double[]{0.05,0.1})
                 .build();
+
 
         RegressionEvaluator regressionEvaluator1 = new RegressionEvaluator()
                 .setLabelCol("load")
@@ -101,18 +104,17 @@ public class LoadForecasting {
                 .setEvaluator(regressionEvaluator3)
                 .setEvaluator(regressionEvaluator4)
                 .setEstimatorParamMaps(paramGridBuilder)
-                .setNumFolds(5) // Use 3+ in practice
-                .setParallelism(5);// Evaluate up to 2 parameter settings in parallel
+                .setNumFolds(3) // Use 3+ in practice
+                .setParallelism(10);// Evaluate up to 2 parameter settings in parallel
         long start = System.currentTimeMillis();
         CrossValidatorModel crossValidatorModel = crossValidator.fit(trainingData);
         long end = System.currentTimeMillis();
-
 
         Dataset<Row> prediction = crossValidatorModel.transform(testData);
 
         prediction.select("prediction", "load", "features").show(5);
 
-        ParamMap paramMap = crossValidatorModel.paramMap();
+//        ParamMap paramMap = crossValidatorModel.paramMap();
 
 
         double rmse = regressionEvaluator1.evaluate(prediction);
@@ -124,7 +126,49 @@ public class LoadForecasting {
         System.out.println("(r2) on test data = " + r2);
         System.out.println("(mae) on test data = " + mae);
 
-        System.out.println("paramMap.toString()======" + paramMap.toString());
+
+        PipelineModel bestModel = (PipelineModel)crossValidatorModel.bestModel();
+        Transformer[] stages = ((PipelineModel) crossValidatorModel.bestModel()).stages();
+
+        System.out.println("bestModel.stages().length=====" + bestModel.stages()[0].extractParamMap().toString());
+
+
+
+//        ParamMap paramMap = bestModel.paramMap();
+//        System.out.println("paramMap.toString()========" + paramMap.toString());
+//
+//        Model<?> bestModel1 = crossValidatorModel.bestModel();
+//        ParamMap[] estimatorParamMaps = crossValidatorModel.getEstimatorParamMaps();
+//        double[] metrics = crossValidatorModel.avgMetrics();
+//        for (ParamMap estimatorParamMap:estimatorParamMaps) {
+//            System.out.println("estimatorParamMap.toString()========" + estimatorParamMap.toString());
+//        }
+//        for (double metric: metrics) {
+//            System.out.println("metrics===" + metric);
+//        }
+
+
+//        List<ParamPair<?>> paramPairs = paramMap.toList();
+//        for (ParamPair paramPair:paramPairs) {
+//            System.out.println("paramPair.toString()========" + paramPair.toString());
+//            Param param = paramPair.param();
+//            System.out.println("param.toString()========" + param.toString());
+//        }
+//        System.out.println("paramMap.toString()======" + paramMap.toString());
+//        ParamMap paramMap = pipeline.paramMap();
+//        List<ParamPair<?>> paramPairs = paramMap.toList();
+//        for (ParamPair paramPair:paramPairs) {
+//            System.out.println("paramPair.toString()========" + paramPair.toString());
+//        }
+
+//        ParamMap paramMap1 = lightGBMRegressor.paramMap();
+//        List<ParamPair<?>> paramPairs1 = paramMap1.toList();
+//        for (ParamPair paramPair:paramPairs1) {
+//            System.out.println("paramPair.toString()========" + paramPair.toString());
+//            Param param = paramPair.param();
+//            System.out.println("param.toString()========" + param.toString());
+//        }
+
         System.out.println("train-time= " + (end-start)/1000.0 + " s");
         spark.stop();
 
